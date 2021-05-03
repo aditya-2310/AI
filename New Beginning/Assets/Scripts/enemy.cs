47b6704b83d5ffa716560a2686e6ed9cc1d4b688
Aditya Kumar <aditya231004@gmail.com>
2021-05-03 13:03:03 -0700

    Enemy deatheffect added

--- New Beginning/Assets/Scripts/enemy.cs
+++ New Beginning/Assets/Scripts/enemy.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class enemy : MonoBehaviour
 {
     public Transform player;
     public GameObject health_status;
     public health_slider_enemy healthSlider;
+    public GameObject deatheffect;
 
     private bool isfacingright;
     public float health = 100;
@@ -30,10 +32,6 @@ public class enemy : MonoBehaviour
             isfacingright = !isfacingright;
             Flip();
         }
-
-        health_on_screen.text = health.ToString();
-
-        healthSlider.setMaxHealth(health);
     }
 
     private void Flip()
@@ -44,6 +42,10 @@ public class enemy : MonoBehaviour
     public void takeDamage(float damage)
     {
         health -= damage;
+        health_on_screen.text = health.ToString();
+
+        healthSlider.setMaxHealth(health);
+
         if (health <= 0)
         {
             Die();
@@ -52,6 +54,7 @@ public class enemy : MonoBehaviour
 
     private void Die()
     {
-        Destroy(gameObject);       
-    }
+        Destroy(gameObject);
+        Instantiate(deatheffect, transform.position, transform.rotation);
+    }    
 }
