f556dce8d4573669ff8500cf853fa6ccd9c4df3d
Aditya Kumar <aditya231004@gmail.com>
2021-05-04 07:39:53 -0700

    Features added

---
+++ New Beginning/Assets/Scripts/menu.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menu : MonoBehaviour
+{
+    public void play()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void quit()
+    {
+        Application.Quit();
+    }
+}
